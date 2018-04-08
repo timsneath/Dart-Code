fe423599a28ad7a6fd435b1ef38509008bdc459e
Danny Tuppeny <danny@tuppeny.com>
2018-04-08 12:14:19 +0100

    Run less tests while debugging

--- test/test_all.ts
+++ test/test_all.ts
@@ -110,8 +110,8 @@ async function runTests(testFolder: string, workspaceFolder: string, sdkPaths: s
 }
 
 async function runAllTests(): Promise<void> {
-	const codeVersions = ["*", "insiders"];
-	const sdkPaths = [process.env.PATH_STABLE || process.env.PATH, process.env.PATH_UNSTABLE].filter((p) => p);
+	const codeVersions = ["*"/*, "insiders"*/];
+	const sdkPaths = [process.env.PATH_STABLE/* || process.env.PATH, process.env.PATH_UNSTABLE*/].filter((p) => p);
 	let runNumber = 1;
 	for (const codeVersion of codeVersions) {
 		for (const sdkPath of sdkPaths) {
