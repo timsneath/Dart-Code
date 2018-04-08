afae523b59e70bb2b9fd3dc1128cce0f45183ce0
Danny Tuppeny <danny@tuppeny.com>
2018-04-08 17:10:51 +0100

    Remove unnecessary import

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,3 @@
-import * as path from "path";
 import { workspace, WorkspaceConfiguration, version as codeVersion, Uri, ConfigurationTarget } from "vscode";
 import { versionIsAtLeast, resolveHomePath } from "./utils";
 
