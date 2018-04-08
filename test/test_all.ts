d95d3f5b257d6494e5068ca0130edc1c26a96f38
Danny Tuppeny <danny@tuppeny.com>
2018-04-08 17:08:15 +0100

    Remove logging

--- test/test_all.ts
+++ test/test_all.ts
@@ -74,7 +74,6 @@ async function runTests(testFolder: string, workspaceFolder: string, sdkPaths: s
 	env.DC_TEST_LOGS = path.join(cwd, ".dart_code_logs", `${testFolder.replace("/", "_")}_${dartFriendlyName}_${codeFriendlyName}`);
 	env.COVERAGE_OUTPUT = path.join(cwd, ".nyc_output", `${testFolder.replace("/", "_")}_${dartFriendlyName}_${codeFriendlyName}.json`);
 	env.TEST_XML_OUTPUT = path.join(cwd, ".test_results", `${testFolder.replace("/", "_")}_${dartFriendlyName}_${codeFriendlyName}.xml`);
-	console.log(`Logging to ${env.DC_TEST_LOGS}`);
 
 	// Ensure any necessary folders exist.
 	if (!fs.existsSync(".nyc_output"))
